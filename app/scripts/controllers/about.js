8cd4adefcf1431cc6cda33f5b9ea750baacbae1c
humancatfood <janborchers09@googlemail.com>
2015-10-15 02:30:07 +0100

    [+] initial project setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name workAngelApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the workAngelApp
+ */
+angular.module('workAngelApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name workAngelApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the workAngelApp
+ */
+angular.module('workAngelApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
